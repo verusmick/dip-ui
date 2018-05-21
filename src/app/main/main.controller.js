758faaec52be071bd94312c71d77721405732381
Micky <miguel.angel.angulo72@gmail.com>
2018-05-20 23:55:20 -0400

    added mqtt module.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -13,15 +13,36 @@
     // var port = "1883";
     var id = "test";
 
-    MqttClient.init(ip, port, id);
-    MqttClient.connect({onSuccess: successCallback});
+    // MqttClient.init(ip, port, id);
+    // MqttClient.connect({onSuccess: successCallback});
+    //
+    // function successCallback() {
+    //   MqttClient.subscribe('CoreElectronics/test');
+    //   var message = new Paho.MQTT.Message("Hello davy");
+    //   message.destinationName = "CoreElectronics/test";
+    //   MqttClient.send(message);
+    // }
+
+
+
+    var client = new Paho.MQTT.Client(ip, Number(port), id);
+
+    client.onConnectionLost = function (resp) {
+      console.log("Lost", resp);
+    };
+
+    client.onMessageArrived = function (message){
+      console.log("onMessageArrived:"+message.payloadString);
+    };
+
+    client.connect({onSuccess: successCallback});
 
     function successCallback() {
-      MqttClient.subscribe('CoreElectronics/test');
-      var message = new Paho.MQTT.Message("Hello davy");
-      message.destinationName = "CoreElectronics/test";
-      MqttClient.send(message);
+      client.subscribe('CoreElectronics/test');
+      // MqttClient.subscribe('CoreElectronics/test');
+      // var message = new Paho.MQTT.Message("Hello davy");
+      // message.destinationName = "CoreElectronics/test";
+      // MqttClient.send(message);
     }
-
   }
 })();
