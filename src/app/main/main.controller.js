c08ea006d8568a3885f8b301cc82a6e3e1f1dd30
Micky <miguel.angel.angulo72@gmail.com>
2018-05-20 23:17:41 -0400

    added mqtt module.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,34 +6,22 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
-    var vm = this;
+  function MainController($timeout,MqttClient) {
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1526859650368;
-    vm.showToastr = showToastr;
+    var ip = "broker.hivemq.com";
+    var port = "8000";
+    // var port = "1883";
+    var id = "test";
 
-    activate();
+    MqttClient.init(ip, port, id);
+    MqttClient.connect({onSuccess: successCallback});
 
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
+    function successCallback() {
+      MqttClient.subscribe('CoreElectronics/test');
+      var message = new Paho.MQTT.Message("Hello davy");
+      message.destinationName = "CoreElectronics/test";
+      MqttClient.send(message);
     }
 
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
   }
 })();
